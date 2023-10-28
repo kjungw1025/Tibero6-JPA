5b550e67ed638334f4d3705ffa9b06551ff5716f
kjungw1025 <kjungw1025@naver.com>
2023-10-28 13:31:54 +0900

    conf: 티베로 연동 환경설정

---
+++ tibero6jpa/src/main/java/taba/tibero6jpa/Tibero6jpaApplication.java
@@ -0,0 +1,13 @@
+package taba.tibero6jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tibero6jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tibero6jpaApplication.class, args);
+	}
+
+}
---
+++ tibero6jpa/src/test/java/taba/tibero6jpa/Tibero6jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package taba.tibero6jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tibero6jpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
